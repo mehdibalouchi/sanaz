b09e8861b0977afe42bb023c77cd6fabb09a7e40
farhoud <farhoud.m7@gmail.com>
2019-04-19 19:25:25 +0430

    Moving actions from chat module to root state

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,8 @@
 import * as types from './mutation-types';
-import { getCommandSuggetions, getInputSuggetions, getSample } from '../../../services/suggestion';
+import { getCommandSuggetions, getInputSuggetions, getSample } from '../services/suggestion';
+import { processText } from '../services/chat-bot';
+import uuid4 from 'uuid/v4';
+import moment from 'moment'
 
 // actions
 export const loadCommandSuggestions = function({ commit, state }) {
@@ -32,4 +35,53 @@ export const setSugesstedInput = function({ dispatch, commit, state, getters }) 
     dispatch('loadInputSuggestions');
   }
 
+};
+
+export const sendTextUserMessage = function({ commit, state, dispatch }, messageText) {
+  let id = uuid4();
+  let now = new moment().toISOString();
+  let userMessage = {
+    ...messageCaseClass,
+    id: id,
+    content: messageText,
+    contentType: 'text',
+    state: null,
+    createdDatetime: now,
+  };
+  commit(types.ADD_MESSAGE, userMessage);
+  dispatch('processTextMessage', userMessage);
+};
+
+export const sendTextBotMessage = function({ commit, state, getters }, { message, messageType }) {
+  let id = uuid4();
+  let now = new moment().toISOString();
+  let botMessage = {
+    ...messageCaseClass,
+    id: id,
+    content: message,
+    contentType: messageType,
+    sender: 'bot',
+    state: null,
+    createdDatetime: now,
+  };
+  commit(types.ADD_MESSAGE, botMessage);
+};
+
+export const processTextMessage = function({ commit, state, dispatch }, userMessage) {
+  let result = processText(userMessage.content);
+  commit(types.REMOVE_MESSAGE, userMessage.id);
+  commit(types.ADD_MESSAGE, { ...userMessage, status: true });
+  if (result.message && result.messageType) {
+    dispatch('sendTextBotMessage', { message: result.message, messageType: result.messageType });
+  }
+
+};
+
+const messageCaseClass = {
+  id: 0,
+  content: '',
+  contentType: 'text',
+  sender: 'user',
+  status: null,
+  createdDatetime: null,
 };
