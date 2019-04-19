7bab1aae7603c0cbb82b0c752bbcbea4dab00fc7
farhoud <farhoud.m7@gmail.com>
2019-04-19 19:27:09 +0430

    Moving getters from chat module to root state

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 // // getters
 export const getSuggestedCommand = function(state) {
   if (state.input.length < 2 || state.commandSuggestions.length === 0) {
@@ -15,4 +17,8 @@ export const getSuggestedItem = function(state) {
 
 export const getSuggestedInput = function(state) {
   return state.input.substring(0, state.input.lastIndexOf(' ')) + ' ' + (state.inputSuggestions.length > 0 ? state.inputSuggestions[0] : '');
+};
+
+export const sortedHistory = function(state) {
+  return state.history.sort((item1, item2) => moment(item1.createdDatetime).diff(moment(item2.createdDatetime), 'milliseconds'));
 };
--- src/store/modules/chat/getters.js
+++
@@ -1,6 +0,0 @@
-import moment from 'moment'
-// getters
-
-export const sortedHistory = function(state) {
-  return state.history.sort((item1,item2)=>moment(item1.createdDatetime).diff(moment(item2.createdDatetime), "milliseconds"))
-}
