33992fbecea42f8533d99b2791d199a3b69a7b01
farhoud <farhoud.m7@gmail.com>
2019-04-19 19:29:49 +0430

    Mutations Moved from chat module to root

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -2,3 +2,5 @@ export const INPUT_CHANGED = 'inputChanged';
 export const INPUT_TYPE_CHANGED = 'inputTypeChanged';
 export const LOUD_INPUT_SUGGESTION = 'loudInputSuggestion';
 export const LOUD_COMMAND_SUGGESTION = 'loudCommandSuggestion';
+export const ADD_MESSAGE = 'addMessage';
+export const REMOVE_MESSAGE = 'messagePrccessed';
